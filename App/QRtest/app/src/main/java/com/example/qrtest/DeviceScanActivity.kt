65f078f5d29cf364ca1382c23413a59e0153df27
choi3179 <66417882+choi3179@users.noreply.github.com>
2021-03-01 17:39:06 +0900

    테스트 코드 수정
    QRtest 커밋 내용 수정
    BLEtest 커밋 내용 수정

---
+++ App/QRtest/app/src/main/java/com/example/qrtest/DeviceScanActivity.kt
@@ -0,0 +1,13 @@
+package com.example.qrtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeviceScanActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_device_scan)
+    }
+}
