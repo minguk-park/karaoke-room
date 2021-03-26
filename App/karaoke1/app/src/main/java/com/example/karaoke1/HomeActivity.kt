a610e2bacbdeea69a01746728897a2507a827f23
choi3179 <66417882+choi3179@users.noreply.github.com>
2021-03-27 02:07:00 +0900

    UI(03.27)
    하단 네비게이션바, 프래그먼트 적용 시작

--- App/karaoke1/app/src/main/java/com/example/karaoke1/HomeActivity.kt
+++ App/karaoke1/app/src/main/java/com/example/karaoke1/HomeActivity.kt
@@ -3,9 +3,14 @@ package com.example.karaoke1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.view.*
 import kotlinx.android.synthetic.main.payment_list_dialog.*
 import kotlinx.android.synthetic.main.payment_list_dialog.view.*
 import kr.co.bootpay.Bootpay
@@ -15,56 +20,65 @@ import kr.co.bootpay.enums.PG
 import kr.co.bootpay.enums.UX
 import kr.co.bootpay.model.BootUser
 
-class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+class HomeActivity : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
 
-        BootpayAnalytics.init(this,"6048ae135b29480027521bb3")
+        val view: View = inflater.inflate(R.layout.activity_home, container, false)
 
-        btnRemote.setOnClickListener {
-            val intent= Intent(this,RemoteActivity::class.java)
-            startActivity(intent)
+        BootpayAnalytics.init(MyApplication.getGlobalApplicationContext(),"6048ae135b29480027521bb3")
+
+        view.btnRemote.setOnClickListener {
+            activity?.let {
+                val intent = Intent(context, RemoteActivity::class.java)
+                startActivity(intent)
+            }
         }
-        btnPayment.setOnClickListener {
-            var price:Int=0
-            val builder= AlertDialog.Builder(this)
-            val dialogView=layoutInflater.inflate(R.layout.payment_list_dialog,null)
-            builder.setView(dialogView).show()
-            dialogView.paymentRg.setOnCheckedChangeListener { radioGroup, checked ->
-                when(checked){
-                    R.id.rb2->{
-                        price=500
-                        dialogView.btnbootpay.text="${price}원 결제"
-                    }
-                    R.id.rb4->{
-                        price=1000
-                        dialogView.btnbootpay.text="${price}원 결제"
-                    }
-                    R.id.rb10->{
-                        price=2000
-                        dialogView.btnbootpay.text="${price}원 결제"
-                    }
-                    R.id.rb20->{
-                        price=2500
-                        dialogView.btnbootpay.text="${price}원 결제"
+        view.btnPayment.setOnClickListener {
+            activity?.let {
+                var price: Int = 0
+                val builder = context?.let { it1 -> AlertDialog.Builder(it1) }
+                val dialogView = layoutInflater.inflate(R.layout.payment_list_dialog, null)
+                builder!!.setView(dialogView)!!.show()
+                dialogView.paymentRg.setOnCheckedChangeListener { radioGroup, checked ->
+                    when (checked) {
+                        R.id.rb2 -> {
+                            price = 500
+                            dialogView.btnbootpay.text = "${price}원 결제"
+                        }
+                        R.id.rb4 -> {
+                            price = 1000
+                            dialogView.btnbootpay.text = "${price}원 결제"
+                        }
+                        R.id.rb10 -> {
+                            price = 2000
+                            dialogView.btnbootpay.text = "${price}원 결제"
+                        }
+                        R.id.rb20 -> {
+                            price = 2500
+                            dialogView.btnbootpay.text = "${price}원 결제"
+                        }
                     }
                 }
-            }
-            dialogView.btnbootpay.setOnClickListener {
-                if(price!=0)
-                    BootpayRequest(price)
-                else
-                    Toast.makeText(this,"곡 수를 선택해주세요.",Toast.LENGTH_SHORT)
+                dialogView.btnbootpay.setOnClickListener {
+                    if (price != 0)
+                        BootpayRequest(price)
+                    else
+                        Toast.makeText(
+                            MyApplication.getGlobalApplicationContext(),
+                            "곡 수를 선택해주세요.",
+                            Toast.LENGTH_SHORT
+                        )
+                }
             }
         }
+        return view
     }
 
     private fun BootpayRequest(price:Int){
         val bootUser= BootUser()
-        Bootpay.init(this)
+        Bootpay.init(MyApplication.getGlobalApplicationContext())
             .setApplicationId("6048ae135b29480027521bb3")
-            .setContext(this)
+            .setContext(MyApplication.getGlobalApplicationContext())
             .setBootUser(bootUser)
             .setUX(UX.PG_DIALOG)
             .setMethod(Method.CARD)
@@ -76,14 +90,14 @@ class HomeActivity : AppCompatActivity() {
                 //서버로 관련 데이터를 전송할 라이프사이클
             }
             .onDone{message->
-                Toast.makeText(this,"결제가 정상적으로 완료되었습니다.",Toast.LENGTH_LONG)
+                Toast.makeText(MyApplication.getGlobalApplicationContext(),"결제가 정상적으로 완료되었습니다.",Toast.LENGTH_LONG)
             }
             .onReady { message-> }
             .onCancel{ message->
-                Toast.makeText(this,"결제가 취소되었습니다.",Toast.LENGTH_LONG)
+                Toast.makeText(MyApplication.getGlobalApplicationContext(),"결제가 취소되었습니다.",Toast.LENGTH_LONG)
             }
             .onError{ message->
-                Toast.makeText(this,"결제 Error",Toast.LENGTH_LONG)
+                Toast.makeText(MyApplication.getGlobalApplicationContext(),"결제 Error",Toast.LENGTH_LONG)
             }
             .onClose{message->}
             .request()
--- App/karaoke1/app/src/main/java/com/example/karaoke1/MainActivity.kt
+++ App/karaoke1/app/src/main/java/com/example/karaoke1/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : AppCompatActivity() {
                         override fun onSuccess(result: MeV2Response) {
                             Toast.makeText(MyApplication.applicationContext(), "Login success", Toast.LENGTH_LONG).show()
                             MyApplication.userEmail=result.kakaoAccount.toString()
-                            val intent=Intent(MyApplication.applicationContext(),HomeActivity::class.java)
+                            val intent=Intent(MyApplication.applicationContext(),MainPageActivity::class.java)
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                             startActivity(MyApplication.applicationContext(),intent,null)
                         }
