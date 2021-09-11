5bc4e8353ac7dcb4929040b1525947631e9a083f
choi3179 <66417882+choi3179@users.noreply.github.com>
2021-09-11 16:55:00 +0900

    image update

---
+++ App/karaoke1/app/src/main/java/com/example/karaoke1/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.karaoke1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+
+class SplashActivity : AppCompatActivity() {
+
+    val SPLASH_VIEW_TIME: Long=2000;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, SPLASH_VIEW_TIME)
+    }
+}
